a8e2b40ddf000d5eb51539199f1e062e07f53c30
Tom Bruyneel <tom.bruyneel@iodigital.com>
2024-06-18 21:27:10 +0200

    add applicationId

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const versionCodeRegexPattern = /(versionCode(?:\s|=)*)(.*)/;
 // versionName — A string used as the version number shown to users [...] -> https://developer.android.com/studio/publish/versioning
 const versionNameRegexPattern = /(versionName(?:\s|=)*)(.*)/;
 
+const applicationIdRegexPattern = /(applicationId(?:\s|=)*)(.*)/;
+
 try {
     const gradlePath = core.getInput('gradlePath');
     const versionCode = core.getInput('versionCode');
     const versionName = core.getInput('versionName');
+    const applicationId = core.getInput('applicationId');
     console.log(`Gradle Path : ${gradlePath}`);
     console.log(`Version Code : ${versionCode}`);
     console.log(`Version Name : ${versionName}`);
+    console.log(`Application Id : ${applicationId}`);
 
     fs.readFile(gradlePath, 'utf8', function (err, data) {
         newGradle = data;
@@ -21,11 +25,17 @@ try {
             newGradle = newGradle.replace(versionCodeRegexPattern, `$1${versionCode}`);
         if (versionName.length > 0)
             newGradle = newGradle.replace(versionNameRegexPattern, `$1\"${versionName}\"`);
+        if (applicationId.length > 0)
+            newGradle = newGradle.replace(applicationIdRegexPattern, `$1\"${applicationId}\"`);
+        
+
         fs.writeFile(gradlePath, newGradle, function (err) {
             if (err) throw err;
             if (versionCode.length > 0)
                 console.log(`Successfully override version code ${versionCode}`)
             if (versionName.length > 0)
+                console.log(`Successfully override version code ${versionName}`)
+            if (applicationId.length > 0)
                 console.log(`Successfully override version code ${versionName}`)
             core.setOutput("result", `Done`);
         });
